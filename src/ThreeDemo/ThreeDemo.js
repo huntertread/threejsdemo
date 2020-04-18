ec3dd4a1d376561ac502f428f093b2419dd4f395
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-04-18 08:53:37 -0700

    added new geometries

--- src/ThreeDemo/ThreeDemo.js
+++ src/ThreeDemo/ThreeDemo.js
@@ -15,15 +15,31 @@ class ThreeDemo extends Component {
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
     this.mount.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+
+    var boxgeometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var boxmaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( boxgeometry, boxmaterial );
     scene.add( cube );
+
+    var pyramidgeometry = new THREE.ConeGeometry( 1, 1, 4 );
+    var pyramidmaterial = new THREE.MeshBasicMaterial( { color: 0xff073a } );
+    var pyramid = new THREE.Mesh( pyramidgeometry, pyramidmaterial );
+    pyramid.position.set(-3, 0, 0);
+    scene.add( pyramid );
+
+    var spheregeometry = new THREE.SphereGeometry( 1, 32, 32 );
+    var spherematerial = new THREE.MeshBasicMaterial( { color: 0xfaed27 } )
+    var sphere = new THREE.Mesh( spheregeometry, spherematerial );
+    sphere.position.set(3, 0, 0);
+    scene.add( sphere );
+
     camera.position.z = 5;
     var animate = function () {
       requestAnimationFrame( animate );
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
+      pyramid.rotation.x += 0.01;
+      pyramid.rotation.y += 0.01;
       renderer.render( scene, camera );
     };
     animate();
